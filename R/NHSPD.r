2085c814a3726381d4b3ee327a24e83b78042e49
Wei Liang Yap <yapweiliang@yahoo.co.uk>
2016-01-17 23:14:23 +0000

    further tweaks and tidying.
    area_codes <- as.list(read_excel(codelist.xl.file, sheet = .codelist.xl.areacodes.sheetname, col_names = FALSE)[,1])$X0 # X1 for .xls, X0 for .xlsx
    
    remove the $X0 reference from above as now seems to not require it.
    
    tweak wording of license/citation

--- R/NHSPD.r
+++ R/NHSPD.r
@@ -1,20 +1,27 @@
 # NHSPD.r
 # NHS Postcode Directory is a specially tailored dataset based on ONS Postcode Directory
 #
+# on comparing NHSPD data (November 2015) with OS CodePoint Open 2015.4.0 (October 2015),
+# at least the England postcodes with grid coordinates are identical to OS CodePoint
+# except for 4 postcodes where NHSPD data seems correct if plotted on map
+# (TR210AA, TR210PR, TR210PW, TR210WY labelled as Isles of Scilly on CodePoint vs Cornwall on NHSPD)
+# therefore this can now supercede my r codepoint library
 
 # http://systems.hscic.gov.uk/data/ods/datadownloads/onsdata
 # http://systems.hscic.gov.uk/data/ods/datadownloads/onsdata/zip-files/gridall.zip
 
-.citations <- function() {
-  s1 <- "Contains OS data (C) Crown copyright and database right [year]"
-  s2 <- "Contains Royal Mail data (C) Royal Mail copyright and database right [year]"
-  s3 <- "Contains National Statistics data (C) Crown copyright and database right [year]"
+# TODO: error checking for CodePoint stuff
+
+.citations <- function(year = 2015) {
+  s1 <- paste0("Contains Ordnance Survey data (C) Crown copyright and database right ", year)
+  s2 <- paste0("Contains Royal Mail data (C) Royal Mail copyright and database right ", year)
+  s3 <- paste0("Contains National Statistics data (C) Crown copyright and database right ", year)
   return(sprintf("The NHSPD package process data that needs the following citations:\n  %s\n  %s\n  %s\n",s1,s2,s3))
 }
 
 # default source data files -----------------------------------------------
 
-.default_gridall_DataFile <- "H:/DATASETS/HSCIC/gridall.csv"
+.default_gridall_DataFile <- "H:/DATASETS/HSCIC/gridall.csv" # local copy of download
 .default_gridall_URL <- "http://systems.hscic.gov.uk/data/ods/datadownloads/onsdata/zip-files/gridall.zip"
 .default_gridall_RDS <- "H:/DATASETS/HSCIC/gridall.RDS"
 
@@ -23,61 +30,56 @@
 # constants ---------------------------------------------------------------
 
 .headers.short <- c(
-  "PCD2", #	Unit postcode – 8 character version
-  "PCDS", #	Unit postcode - variable length (e-Gif) version
-  "DOINTR", #	Date of introduction
-  "DOTERM", #	Date of termination
+  "PCD2",       #	Unit postcode – 8 character version
+  "PCDS",       #	Unit postcode - variable length (e-Gif) version
+  "DOINTR",     #	Date of introduction
+  "DOTERM",     #	Date of termination
   "OSEAST100M", #	National grid reference - Easting
   "OSNRTH100M", #	National grid reference - Northing
-  "OSCTY", #	County
-  "ODSLAUA", #	Local Authority Organisation
-  "OSLAUA", #	Local authority district (LAD)/unitary authority (UA)/ metropolitan district (MD)/ London borough (LB)/ council area (CA)/district council area (DCA)
-  "OSWARD", #	(Electoral) ward/division
-  "USERTYPE", #	Postcode user type
-  "OSGRDIND", #	Grid reference positional quality indicator
-  "CTRY", #	Country
-  "OSHLTHAU", #	Former Strategic health authority (SHA)/ health board (HB)/ health authority (HA)/ health & social care board (HSCB)
-  "GOR", #	Region
-  "OLDHA", #	Pre-2002 Health Authority
-  "NHSCR", #	Commissioning Region
-  "CCG", #	Clinical Commissioning Group (CCG)/ local health board (LHB)/ community health partnership (CHP)/ local commission-ing group (LCG)/ primary healthcare directorate (PHD)
-  "PSED", #	1991 Census Enumeration District (ED)
-  "CENED", #	1991 Census Enumeration District (ED)
-  "EDIND", #	ED positional quality indicator
-  "WARD98", #	1998 ward
-  "OA01", #	2001 Census Output Area
-  "NHSRG", #	NHS Region (Geography), formerly Area Team
-  "HRO", #	Former Pan SHA
-  "LSOA01", #	2001 Lower Layer Super Output Area (LSOA)
-  "UR01IND", #	2001 Census Urban/rural indicator
-  "MSOA01", #	2001 Census Middle Layer Super Output Area (MSOA)
-  "CANNET", #	Former Cancer Network
-  "SCN", #	Strategic Clinical Network
-  "OSHAPREV", #	‘First wave’ Strategic health authority (SHA)/ health board (HB)/ health authority (HA)/ health and social services board (HSSB)
-  "OLDPCT", #	‘First wave’ Primary Care Trust (PCT)/ Local Health Board (LHB)/ Care Trust (CT)
-  "OLDHRO", #	‘Old’ IT Cluster (ITC)
-  "PCON", #	Westminster parliamentary constituency
-  "CANREG", #	Cancer Registry
-  "PCT", #	‘Second wave’ Primary Care Trust (PCT)/ Care Trust/ Care Trust Plus (CT)/ local health board (LHB)/ community health partnership (CHP)/ local commission-ing group (LCG)/ primary healthcare directorate (PHD)
-  "OSEAST1M", #	National grid reference - Easting
-  "OSNRTH1M", #	National grid reference - Northing
-  "OA11", #	2011 Census output area
-  "LSOA11", #	2011Census  lower layer super output area (LSOA)
-  "MSOA11" #	2011 Census middle layer super output area (MSOA)
+  "OSCTY",      #	County
+  "ODSLAUA",    #	Local Authority Organisation
+  "OSLAUA",     #	Local authority district (LAD)/unitary authority (UA)/ metropolitan district (MD)/ London borough (LB)/ council area (CA)/district council area (DCA)
+  "OSWARD",     #	(Electoral) ward/division
+  "USERTYPE",   #	Postcode user type
+  "OSGRDIND",   #	Grid reference positional quality indicator
+  "CTRY",       #	Country
+  "OSHLTHAU",   #	Former Strategic health authority (SHA)/ health board (HB)/ health authority (HA)/ health & social care board (HSCB)
+  "GOR",        #	Region
+  "OLDHA",      #	Pre-2002 Health Authority
+  "NHSCR",      #	Commissioning Region
+  "CCG",        #	Clinical Commissioning Group (CCG)/ local health board (LHB)/ community health partnership (CHP)/ local commission-ing group (LCG)/ primary healthcare directorate (PHD)
+  "PSED",       #	1991 Census Enumeration District (ED)
+  "CENED",      #	1991 Census Enumeration District (ED)
+  "EDIND",      #	ED positional quality indicator
+  "WARD98",     #	1998 ward
+  "OA01",       #	2001 Census Output Area
+  "NHSRG",      #	NHS Region (Geography), formerly Area Team
+  "HRO",        #	Former Pan SHA
+  "LSOA01",     #	2001 Lower Layer Super Output Area (LSOA)
+  "UR01IND",    #	2001 Census Urban/rural indicator
+  "MSOA01",     #	2001 Census Middle Layer Super Output Area (MSOA)
+  "CANNET",     #	Former Cancer Network
+  "SCN",        #	Strategic Clinical Network
+  "OSHAPREV",   #	‘First wave’ Strategic health authority (SHA)/ health board (HB)/ health authority (HA)/ health and social services board (HSSB)
+  "OLDPCT",     #	‘First wave’ Primary Care Trust (PCT)/ Local Health Board (LHB)/ Care Trust (CT)
+  "OLDHRO",     #	‘Old’ IT Cluster (ITC)
+  "PCON",       #	Westminster parliamentary constituency
+  "CANREG",     #	Cancer Registry
+  "PCT",        #	‘Second wave’ Primary Care Trust (PCT)/ Care Trust/ Care Trust Plus (CT)/ local health board (LHB)/ community health partnership (CHP)/ local commission-ing group (LCG)/ primary healthcare directorate (PHD)
+  "OSEAST1M",   #	National grid reference - Easting
+  "OSNRTH1M",   #	National grid reference - Northing
+  "OA11",       #	2011 Census output area
+  "LSOA11",     #	2011Census  lower layer super output area (LSOA)
+  "MSOA11"      #	2011 Census middle layer super output area (MSOA)
 )
 
 .headers.wanted <- c(
   "PCD2", #	Unit postcode – 8 character version
-#  "PCDS", #	Unit postcode - variable length (e-Gif) version
-#  "DOINTR", #	Date of introduction
   "DOTERM", #	Date of termination
-#  "OSEAST100M", #	National grid reference - Easting
-#  "OSNRTH100M", #	National grid reference - Northing
   "OSCTY", #	County
 #  "ODSLAUA", #	Local Authority Organisation
   "OSLAUA", #	Local authority district (LAD)/unitary authority (UA)/ metropolitan district (MD)/ London borough (LB)/ council area (CA)/district council area (DCA)
 #  "OSWARD", #	(Electoral) ward/division
-#  "USERTYPE", #	Postcode user type
   "OSGRDIND", #	Grid reference positional quality indicator
   "CTRY", #	Country
 #  "OSHLTHAU", #	Former Strategic health authority (SHA)/ health board (HB)/ health authority (HA)/ health & social care board (HSCB)
@@ -98,7 +100,7 @@
 )
 
 # CodePoint stuff
-.codelist.xl.filename <- "Codelist.xlsx" # 2015.4.0 version uses .xlsx; .xlsx needs $X0, not $X1
+.codelist.xl.filename <- "Codelist.xlsx" # 2015.4.0 version uses .xlsx
 .codelist.xl.areacodes.sheetname <- "AREA_CODES" # name of sheet within Codelist.xls that has table of the different types of codes
 .nhscodelist.xl.filename <- "NHS_Codelist.xls"
 
@@ -120,7 +122,7 @@
 #'
 #' @param codepoint.doc.path full path to where we can find the unzipped xls/xlsx files that contain area code names
 #'
-#' @return compiled list of all the area codes
+#' @return data.frame compiled list of all the area codes
 #' @export
 #'
 #' @examples
@@ -132,23 +134,21 @@ getCodePointAreaCodes <- function( codepoint.doc.path = paste(.default_CodePoint
   # TODO error checking of path syntax
 
   # compile all the area codes
-  area_codes <- as.list(read_excel(codelist.xl.file, sheet = .codelist.xl.areacodes.sheetname, col_names = FALSE)[,1])$X0 # X1 for .xls, X0 for .xlsx
+  area_codes <- as.list(read_excel(codelist.xl.file, sheet = .codelist.xl.areacodes.sheetname, col_names = FALSE)[,1])
   area_codes <- lapply( area_codes, .read_codelist,  path = codelist.xl.file )
   area_codes <- do.call(rbind, area_codes)
   names(area_codes) <- c("AreaName", "AreaID", "AreaType")
+
   nhs_areas <- excel_sheets(nhscodelist.xl.file)
   nhs_areas <- lapply( nhs_areas, .read_codelist, path = nhscodelist.xl.file, NHS = TRUE )
   nhs_areas <- do.call(rbind, nhs_areas)
   names(nhs_areas) <- c("AreaID", "AreaName", "AreaType")
+
   CodePointAreaCodes <- rbind(area_codes,nhs_areas)
   CodePointAreaCodes  <- CodePointAreaCodes[complete.cases(CodePointAreaCodes),]
 
   return(CodePointAreaCodes)
 }
-
-
-
-
 
 # getNHSPD ----------------------------------------------------------------
 
@@ -163,10 +163,11 @@ getCodePointAreaCodes <- function( codepoint.doc.path = paste(.default_CodePoint
 #' @param gridall.RDS full path to saved (processed) data
 #' @param force.download Force download of file from source
 #' @param remake.RDS Rebuild and save the data
-#' @param codepoint.doc.path see getCodePointAreaCodes()
-#' @param CodePoint.compatible when remake.RDS - column names to be compatible with my previous CodePoint library
+#' @param codepoint.doc.path if \code{remake.RDS==TRUE} - specify where to find unzipped OS CodePoint documentation - see getCodePointAreaCodes()
+#' @param codepoint.compatible if \code{remake.RDS==TRUE} - column names to be compatible with my previous codepoint library
+#' @param only.England if \code{remake.RDS==TRUE} - only return rows applicable to England
 #'
-#' @return data table
+#' @return data.table
 #' @export
 #'
 #' @examples NHSPD <- getNHSPD()
@@ -175,7 +176,7 @@ getNHSPD <- function( gridall.file = .default_gridall_DataFile,
                       gridall.RDS = .default_gridall_RDS,
                       force.download = FALSE,
                       remake.RDS = FALSE,
-                      CodePoint.compatible = TRUE,
+                      codepoint.compatible = TRUE,
                       codepoint.doc.path = paste(.default_CodePoint_Unzipped_Path, "Doc", sep = "/"),
                       only.England = TRUE ) {
 
@@ -240,12 +241,18 @@ getNHSPD <- function( gridall.file = .default_gridall_DataFile,
     z <- subset(z, CTRY == "E92000001")
   }
 
-  if (TRUE) { # remove terminated postcodes
+  if (FALSE) {
+    # remove non-geographic postcodes
+    # TODO ?
+  }
+
+  if (TRUE) {
+    # remove terminated postcodes
     z <- subset(z, DOTERM == "")
     z[, DOTERM:=NULL] # drop this column
   }
 
-  if (CodePoint.compatible) {
+  if (codepoint.compatible) {
     setnames(z, c("OSEAST1M","OSNRTH1M"), c("Eastings", "Northings"))
     z[, Eastings:=as.integer(Eastings)]
     z[, Northings:=as.integer(Northings)]
@@ -259,7 +266,7 @@ getNHSPD <- function( gridall.file = .default_gridall_DataFile,
     message("getNHSPD() obtaining area names from local copy of OS CodePoint...")
     CodePointAreaCodes <- as.data.table(getCodePointAreaCodes(codepoint.doc.path))
 
-    # join  2,145,393
+    # join
     message("getNHSPD() joining areanames with NHSPD...")
     z <- merge(z, CodePointAreaCodes, by.x = "OSLAUA", by.y = "AreaID", all.x = TRUE)
   }
